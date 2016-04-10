13f0ab52f383e589f482d88781449ae8598f47d8
Kevin Njeru <knjeru@gmail.com>
2016-04-10 17:21:43 -0600

    Add skeleton function for adding to cart

--- src/client/app/home/controller.js
+++ src/client/app/home/controller.js
@@ -10,10 +10,16 @@
 
     function HomeCtrl ($stateParams, $location, catalogApiService) {
       var vm = this;
+      vm.cartItem = {};
 
       catalogApiService.getAllItems()
       .success(function(data) {
         console.log(data);
       });
+
+      // Add item to user cart
+      vm.addItem = function(id, body) {
+
+      };
     }
 })();

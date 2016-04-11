a1c7e43183399cfd4cfd48f8a8ff92213e880421
Kevin Njeru <knjeru@gmail.com>
2016-04-11 07:36:34 -0600

    User can order by price

--- src/client/app/home/controller.js
+++ src/client/app/home/controller.js
@@ -14,7 +14,8 @@
 
       catalogApiService.getAllItems()
       .success(function(data) {
-        console.log(data);
+        console.log(data[0]);
+        vm.catalog = data;
       });
 
       // Add item to user cart
@@ -24,5 +25,42 @@
           console.log(data);
         });
       };
+
+      vm.addQuantity =
+        [{"value": 1},
+          {"value": 2},
+          {"value": 3},
+          {"value": 4},
+          {"value": 5},
+          {"value": 6},
+          {"value": 7},
+          {"value": 8},
+          {"value": 9},
+          {"value": 10}];
+
+      vm.category =
+      [{"category": "spring"},
+        {"category": "warm"},
+        {"category": "winter"},
+        {"category": "lucid"},
+        {"category": "hot"},
+        {"category": "dry"},
+        {"category": "summer"},
+        {"category": "awesome"},
+        {"category": "cold"},
+        {"category": "dark"}];
+
+      vm.sort = false;
+
+      vm.sortPrice =
+      [{
+          "value": "+price",
+          "label": "Price low to high"
+        },
+        {
+          "value": "-price",
+          "label": "Price high to low"
+        }];
+
     }
 })();
